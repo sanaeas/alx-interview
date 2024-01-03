a448609fc75ae2ba6ca1337c1f5b5fa23b707bcd
sanaeas <sanaassim1@gmail.com>
2024-01-03 16:53:58 +0100

    Fix pycodestyle

--- 0x00-pascal_triangle/0-pascal_triangle.py
+++ 0x00-pascal_triangle/0-pascal_triangle.py
@@ -1,6 +1,7 @@
 #!/usr/bin/python3
 """Return the pascal triangle of a given size"""
 
+
 def pascal_triangle(n):
     """
     Return a list of lists of integers representing the Pascal's triangle of n
