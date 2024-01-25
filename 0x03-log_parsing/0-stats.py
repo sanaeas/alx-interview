3759bb9a3827a0f1095250437eca6120f4917cae
sanaeas <sanaassim1@gmail.com>
2024-01-25 15:58:42 +0100

    Log parsing

--- 0x03-log_parsing/0-stats.py
+++ 0x03-log_parsing/0-stats.py
@@ -2,62 +2,83 @@
 """A script that reads stdin line by line and computes metrics"""
 
 import re
-import sys
-
-
-LOG_PATTERN = (r"^(\S+) ?"
-               r"- ?\[\S+ ?\S+\] "
-               r"\"GET /projects/260 HTTP/1\.1\" "
-               r"(\S+) (\S+)$")
 
 
 def extract(log_line):
     """Extract the status code and size from a log line"""
-    match = re.search(LOG_PATTERN, log_line)
+    fields = (
+        r"\s*(?P<ip>\S+)\s*",
+        r"\s*\[(?P<date>\d+\-\d+\-\d+ \d+:\d+:\d+\.\d+)\]",
+        r'\s*"(?P<request>[^"]*)"\s*',
+        r"\s*(?P<status_code>\S+)",
+        r"\s*(?P<file_size>\d+)"
+    )
 
-    if match:
-        return (match.group(2), match.group(3))
-    else:
-        return None, None
+    info = {
+        "status_code": 0,
+        "file_size": 0,
+    }
+
+    log_format = "{}\\-{}{}{}{}\\s*".format(fields[0],
+                                            fields[1],
+                                            fields[2],
+                                            fields[3],
+                                            fields[4])
+    resp_match = re.fullmatch(log_format, log_line)
+    if resp_match is not None:
+        status_code = resp_match.group("status_code")
+        file_size = int(resp_match.group("file_size"))
+        info["status_code"] = status_code
+        info["file_size"] = file_size
+    return info
 
 
-def print_stats(size, codes):
-    """Print the metrics for the log lines read so far"""
-    print("File size: {}".format(str(size)))
-    for code in sorted(codes.keys()):
-        if codes[code] != 0:
-            print("{}: {}".format(code, str(codes[code])))
+def print_stats(total_size, status_stats):
+    """Print the metrics for the log lines"""
+    print("File size: {:d}".format(total_size), flush=True)
+    for status_code in sorted(status_stats.keys()):
+        num = status_stats.get(status_code, 0)
+        if num > 0:
+            print("{:s}: {:d}".format(status_code, num), flush=True)
 
 
-status_codes = {
-    "200": 0, "301": 0,
-    "400": 0, "401": 0,
-    "403": 0, "404": 0,
-    "405": 0, "500": 0
-}
-size = 0
-lines = 0
+def update_metrics(line, total_size, status_stats):
+    """Updates the metrics"""
+    line_info = extract(line)
+    status_code = line_info.get("status_code", "0")
+    if status_code in status_stats.keys():
+        status_stats[status_code] += 1
+    return total_size + line_info["file_size"]
 
-try:
-    for line in sys.stdin:
-        lines += 1
 
-        code, size = extract(line.strip())
-        if code and size:
-            try:
-                size += int(size)
-            except Exception:
-                pass
-            try:
-                if code in status_codes:
-                    status_codes[code] += 1
-            except Exception:
-                pass
+def execute():
+    """Start the log parsing"""
+    line_num = 0
+    total_size = 0
+    status_stats = {
+        "200": 0,
+        "301": 0,
+        "400": 0,
+        "401": 0,
+        "403": 0,
+        "404": 0,
+        "405": 0,
+        "500": 0,
+    }
+    try:
+        while True:
+            line = input()
+            total_size = update_metrics(
+                line,
+                total_size,
+                status_stats,
+            )
+            line_num += 1
+            if line_num % 10 == 0:
+                print_stats(total_size, status_stats)
+    except (KeyboardInterrupt, EOFError):
+        print_stats(total_size, status_stats)
 
-        if lines % 10 == 0 and lines != 0:
-            print_stats(size, status_codes)
-except KeyboardInterrupt:
-    pass
 
-finally:
-    print_stats(size, status_codes)
+if __name__ == "__main__":
+    execute()
